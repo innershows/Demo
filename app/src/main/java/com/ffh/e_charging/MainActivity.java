6da3550f236683d77b9080ba628a78ecf97c885c
innershows <innershow@gmail.com>
2015-11-26 15:38:17 +0800

    test

--- app/src/main/java/com/ffh/e_charging/MainActivity.java
+++ app/src/main/java/com/ffh/e_charging/MainActivity.java
@@ -3,9 +3,10 @@ package com.ffh.e_charging;
 import com.ffh.e_charging.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
-    
+
     @Override
     public int initView() {
+        System.out.println("=======>>>>>");
         return R.layout.activity_main;
     }
 }
